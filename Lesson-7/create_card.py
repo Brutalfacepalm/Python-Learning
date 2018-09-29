af07b82c3dfd9d7a3073828fc1b4d93ba260d824
Brutalfacepalm <theshick@mail.ru>
2018-09-29 20:45:39 +0400

    Оптимизировал заполнение карточек

--- Lesson-7/create_card.py
+++ Lesson-7/create_card.py
@@ -1,37 +1,33 @@
 import random
 
 def fill(rows, cols):
-    row, numbers, card = set(), [], []
+    numbers, card = [], []
     # создадим вложенные списки с 3 уникальными числами для каждого столбца карточки
     # в зависимости от разряда стобца
     for c in range(cols):
         if c == 0:
-            while len(row) < rows:
-                row.add(random.randint(1 , 9))
-            numbers.append(list(row))
-            row.clear()
+            numbers.append(random.sample(range(1,9),5))
         elif c == cols - 1:
-            while len(row) < rows:
-                row.add(random.randint(c * 10, (c + 1) * 10))
-            numbers.append(list(row))
-            row.clear()
+            numbers.append(random.sample(range(c * 10, (c + 1) * 10), 5))
         else:
-            while len(row) < rows:
-                row.add(random.randint(c * 10, c * 10 + 9))
-            numbers.append(list(row))
-            row.clear()
+            numbers.append(random.sample(range(c * 10, c * 10 + 9), 5))
 
     for r in range(rows):
         card.append([])
         # создадим последовательность из 5-и номеров непустых ячеек для каждой строчки карточки
-        while len(row) < 5:
-            row.add(random.randint(1, 9))
-        # для каждой строки карточки: при обнаружении непустой ячейки обратиться к списку чисел
-        # и взять элемент подходящий по разряду(с-1) и номеру строки(r)
+        row = random.sample(range(1,10),5)
+        # rows = [i for i in range(1, 10)]
+        # for rs in rows:
+        #     if not rs in row:
+        #         rows[rows.index(rs)] = ' '
+        # print(rows)
+
+
+
         for c in range(1, 10):
             if c in row:
                 card[r].append(numbers[c - 1][r]) # заполняем непустую ячейку числом
             else:
                 card[r].append(' ')
-        row.clear()
+        # row.clear()
     return card
